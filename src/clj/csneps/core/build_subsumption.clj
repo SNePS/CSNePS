c6697a653516fe310def291dabc9cfd9d59ad693
Daniel R. Schlegel <drschleg@buffalo.edu>
2013-10-25 13:39:21 -0400

    Start of some structural subsumption work.

--- src/clj/csneps/core/build_subsumption.clj
+++ src/clj/csneps/core/build_subsumption.clj
@@ -7,14 +7,27 @@
 (defn structurally-subsumes-vars
   "var1 structurally subsumes var2 if var1 has a subset of
    var2's restrictions."
-  [var1 var2])
+  [var1 var2]
+  (set/subset? (:restriction-set var1) (:restriction-set var2)))
 
 (defn structurally-subsumes-terms
-  "term1 structurally subsumes term2 if term2 is in a subset
-   of the relations term1 is."
-  [term1 term2])
+  "term1 structurally subsumes term2 if term1 is in a subset
+   of the relations term2 is."
+  [term1 term2]
+  (let [t1rs @(:up-cablesetw term1)
+        t2rs @(:up-cablesetw term2)]
+    (every? 
+      #(and (t2rs %)
+            (set/subset? (t1rs %) (t2rs %)))
+      (keys t1rs))))
 
 (defn structurally-subsumes-varterm
   "var structurally subsumes term if term is in all of the
    relations that are in var's restriction set."
-  [var term])
+  [var term]
+  (let [rs (:restriction-set var)
+        ucs (:up-cablesetw term)]
+    ))
+
+
+
