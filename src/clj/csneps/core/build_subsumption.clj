19c67cf27f78031b0c418f3337c2647a22e6658c
Daniel R. Schlegel <drschleg@buffalo.edu>
2013-10-31 23:00:48 -0400

    Start of subsumption hierarchy.

--- src/clj/csneps/core/build_subsumption.clj
+++ src/clj/csneps/core/build_subsumption.clj
@@ -4,6 +4,33 @@
 ;;; - Do we want to keep these separate?
 ;;; - What will be the role of unification?
 
+(def subsumption-hierarchy (ref '[]))
+
+(defrecord2 TreeNode 
+  [data nil 
+   children (ref '[])])
+
+(defn restriction-subset?
+  [term1 term2]
+  (set/subset? @(:restriction-set term1) @(:restriction-set term2)))
+
+(defn insert
+  [arb]
+  (loop [parent nil
+         nodes @subsumption-hierarchy]
+    (cond
+      (empty? nodes)
+      (dosync (alter (or (:children parent)
+                         subsumption-hierarchy)
+                     conj (new-tree-node {:data arb})))
+      (restriction-subset? (:data (first nodes)) arb)
+      (recur (first nodes)
+             @(:children (first nodes)))
+      :else
+      (recur parent
+             (rest nodes)))))
+  
+
 (defn structurally-subsumes-vars
   "var1 structurally subsumes var2 if var1 has a subset of
    var2's restrictions."
@@ -25,9 +52,18 @@
   "var structurally subsumes term if term is in all of the
    relations that are in var's restriction set."
   [var term]
-  (let [rs (:restriction-set var)
-        ucs (:up-cablesetw term)]
-    ))
+  (let [rs @(:restriction-set var)
+        ucs (apply concat (map deref (vals @(:up-cablesetw term))))]
+    (every? (fn [r] 
+              (println r ucs)
+              (cond
+                ;; Equivalence is a good enough condition to accept on.
+                (some #(= r %) ucs) true 
+                ;; Recursive case - which is the matching term? This is naive.
+                (arbitraryTerm? r) (some #(structurally-subsumes-varterm r %) ucs)
+                ;; The term r contains a variable at some lower level. We should
+                ;; try to unify r with each item in ucs. (Not yet implemented)
+                (generic-term? r) false)) rs)))
 
 
 
