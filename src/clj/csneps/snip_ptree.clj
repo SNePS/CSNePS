c5a277f3defbcaf63504533420c119f29303c8a7
Daniel R. Schlegel <drschleg@buffalo.edu>
2013-11-02 19:26:13 -0400

    Build PTrees.

--- src/clj/csneps/snip_ptree.clj
+++ src/clj/csneps/snip_ptree.clj
@@ -1,7 +1,8 @@
 (in-ns 'csneps.snip)
 
 (defrecord PTree
-  [tree]
+  [tree
+   pnode-ruiset-map]
   RUIStructure
   (get-rule-use-info [this new-rui]))
 
@@ -38,9 +39,49 @@
                (conj vars-done currvar)
                (concat patseq (filter #(not (some #{%} patseq)) varterms)))))))
     
+(defn share-var?
+  [term1 term2]
+  (not (nil? (intersection (term-vars term1) (term-vars term2)))))
+
 (defn pat-seq-to-ptree
   [patseq patlist]
-  )
+  (loop [ptree '()
+         patseq patseq]
+    (if (empty? patseq)
+      ptree
+      (let [n (first patseq)
+            nn (second patseq)]
+        (if (and
+              (not (empty? ptree))
+              (or (nil? nn)
+                  (not (share-var? n nn))))
+          ;; Combine n with previous pattern.
+          (recur
+            (list ptree n)
+            (rest patseq))
+          ;; Add new pair of n, nn
+          (recur
+            (if (not (empty? ptree))
+              (list ptree (list n nn))
+              (list n nn))
+            (rest (rest patseq))))))))
+
+(defn walk-list
+  [inner outer form]
+  (if (list? form) 
+    (outer (apply list (map inner form)))
+    (outer form)))
+
+(defn postwalk-list 
+  [f form]
+  (walk-list (partial postwalk-list f) f form))
+
+(defn make-pnode-ruiset-map
+  [ptree]
+  (let [m (ref {})]
+    (dosync 
+      (postwalk-list (fn [x] (alter m assoc x (ref #{})) x) ptree))
+    @m))
 
 (defn make-ptree
   [syntype fillers]
@@ -50,4 +91,18 @@
         var-pat-map (apply merge-with merge-var-term (map var-pat-map ants))
         adj-pat-seq (adjacent-pat-seq ants var-pat-map)
         ptree (pat-seq-to-ptree adj-pat-seq ants)]
-    ptree))
+    (PTree. ptree (make-pnode-ruiset-map ptree))))
+
+(defn test-ptree
+  []
+  (dosync (ref-set build/KRNovice true))
+  (let [a (build/assert '(and (A (every x (Isa x v1)) (every y (Isa y v2)))
+                              (B (every z (Isa z v3)) (every w (Isa w v4)))
+                              (C z (every v (Isa v v5)))
+                              (D x z)
+                              (E y v)
+                              (G y z)
+                              (H x w))
+                        (ct/currentContext) :hyp)]
+    (make-ptree :csneps.core/Conjunction (:down-cableset a))))
+  
