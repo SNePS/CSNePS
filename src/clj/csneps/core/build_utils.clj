ce1786b6c967bba6341d8c625adfcf06ce7562e1
Daniel R. Schlegel <drschleg@buffalo.edu>
2014-01-02 17:50:36 -0500

    term-walk now works with frames that have :fsymbols. Separated syntype-fsym-map from the term-predicate function.

--- src/clj/csneps/core/build_utils.clj
+++ src/clj/csneps/core/build_utils.clj
@@ -7,20 +7,21 @@
 (def synvariable? #(or (ignore-variable? %)
                       (and (symbol? %) (re-matches #"^\?.*" (name %)))))
 
+(def syntype-fsym-map {:csneps.core/Negation 'not,
+                       :csneps.core/Negationbyfailure 'thnot,
+                       :csneps.core/Conjunction 'and,
+                       :csneps.core/Disjunction 'or,
+                       :csneps.core/Equivalence 'iff,
+                       :csneps.core/Xor 'xor,
+                       :csneps.core/Nand 'nand,
+                       :csneps.core/Andor 'andor,
+                       :csneps.core/Thresh 'thresh,
+                       :csneps.core/Implication 'if})
+
 (defn term-predicate
   [term]
   (or
-    ((type-of term)
-      {:csneps.core/Negation 'not,
-       :csneps.core/Negationbyfailure 'thnot,
-       :csneps.core/Conjunction 'and,
-       :csneps.core/Disjunction 'or,
-       :csneps.core/Equivalence 'iff,
-       :csneps.core/Xor 'xor,
-       :csneps.core/Nand 'nand,
-       :csneps.core/Andor 'andor,
-       :csneps.core/Thresh 'thresh,
-       :csneps.core/Implication 'if})
+    ((type-of term) syntype-fsym-map)
     (let [p (:print-pattern (:caseframe term))]
       (if (and (seq? (first p)) (= (first (first p)) 'quote))
         (second (first p))
@@ -29,12 +30,17 @@
 (defn term-walk
   [inner outer termpart]
   (cond
-    (molecularTerm? termpart) (outer (build (conj (doall (map inner (:down-cableset termpart))) (term-predicate termpart)) 
-                                        :Proposition 
-                                        {}))
-    ;(arbitraryTerm? termpart) (outer (build-variable (list 'every (:var-label termpart) (map inner @(:restriction-set termpart))))) 
-    (atomicTerm? termpart) (outer termpart)
-    (set? termpart) (set (doall (map inner termpart)))
+    (molecularTerm? termpart) (outer (build 
+                                       (if-let [fsym (or ((type-of termpart) syntype-fsym-map)
+                                                         (let [p (:print-pattern (:caseframe termpart))]
+                                                           (when (and (seq? (first p)) (= (first (first p)) 'quote))
+                                                             (second (first p)))))]
+                                         (conj (doall (map inner (:down-cableset termpart))) fsym)
+                                         (doall (map inner (:down-cableset termpart))))
+                                       :Proposition 
+                                       {}))
+    (atomicTerm? termpart) (do (println (outer termpart)) (outer termpart))
+    (set? termpart) (do (println (set (doall (map inner termpart)))) (set (doall (map inner termpart))))
     :else (error (str "Term contains unknown parts (" termpart ")"))))
 
 (defn term-recur
