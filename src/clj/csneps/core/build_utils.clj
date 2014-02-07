8930dcc27d64c072be15adf88bd7a7fbffcbf404
Daniel R. Schlegel <drschleg@buffalo.edu>
2014-02-07 13:33:19 -0500

    :Propositional instead of :Proposition in term walking.

--- src/clj/csneps/core/build_utils.clj
+++ src/clj/csneps/core/build_utils.clj
@@ -37,7 +37,7 @@
                                                              (second (first p)))))]
                                          (conj (doall (map inner (:down-cableset termpart))) fsym)
                                          (doall (map inner (:down-cableset termpart))))
-                                       :Proposition 
+                                       :Propositional
                                        {}))
     (atomicTerm? termpart) (outer termpart)
     (set? termpart) (set (doall (map inner termpart)))
@@ -47,7 +47,7 @@
   [inner outer termpart]
   (cond
     (molecularTerm? termpart) (outer (build (conj (doall (map inner (:down-cableset termpart))) (term-predicate termpart)) 
-                                        :Proposition 
+                                        :Propositional
                                         {}))
     ;(arbitraryTerm? termpart) (outer (build-variable (list 'every (:var-label termpart) (map inner @(:restriction-set termpart))))) 
     (atomicTerm? termpart) (outer termpart)
