28231336ecfb0a81914d9498087f5fb4507d8e81
Daniel R. Schlegel <drschleg@buffalo.edu>
2014-02-27 19:26:44 -0500

    Sanitize messages so equality works out.

--- src/clj/csneps/snip_linear_msg_set.clj
+++ src/clj/csneps/snip_linear_msg_set.clj
@@ -7,13 +7,14 @@
   MessageStructure
   (get-rule-use-info [this new-msg]
     ;; if new-msg isn't actually new, return empty set.
-    (dosync
-      (if (@(:msg-set this) new-msg)
-        #{}
-        (let [compat-msgs (filter #(compatible? % new-msg) @(:msg-set this))
-              merged-msgs (set (map #(merge-messages new-msg %) compat-msgs))]
-          (alter (:msg-set this) union merged-msgs #{new-msg})
-          (conj merged-msgs new-msg))))))
+    (let [new-msg (sanitize-message new-msg)]
+      (dosync
+        (if (@(:msg-set this) new-msg)
+          #{}
+          (let [compat-msgs (filter #(compatible? % new-msg) @(:msg-set this))
+                merged-msgs (set (map #(merge-messages new-msg %) compat-msgs))]
+            (alter (:msg-set this) union merged-msgs #{new-msg})
+            (conj merged-msgs new-msg)))))))
 
 (defn make-linear-msg-set
   []
