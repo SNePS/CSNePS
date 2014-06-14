2b1bd7ad37c61e01ee5afd3cc1f9d5ae8ea668f1
Daniel R. Schlegel <drschleg@buffalo.edu>
2014-06-13 21:39:16 -0400

    Fixed issue with forward inference and derivative message.

--- src/clj/csneps/snip_message.clj
+++ src/clj/csneps/snip_message.clj
@@ -88,7 +88,7 @@
     (assoc :fwd-infer? (or fwd-infer? (:fwd-infer? message)))
     (assoc :invoke-set (or invoke-set (if origin
                                         @(:future-fw-infer origin)
-                                        @(:future-fw-infer (:origin message)))))
+                                        (when (:origin message) @(:future-fw-infer (:origin message))))))
     (assoc :taskid (or taskid (:taskid message)))
     (assoc :pos (or pos (if (if (nil? true?) (:true? message) true?) 1 0)))
     (assoc :neg (or neg (if (if (nil? true?) (:true? message) true?) 0 1)))
