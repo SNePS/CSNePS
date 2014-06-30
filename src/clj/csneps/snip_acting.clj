2c4cc4adf69083796adb6ee7aadeadd5572c82bc
Daniel R. Schlegel <drschleg@buffalo.edu>
2014-06-30 19:54:42 -0400

    Now using valve selectors instead of open/close valves.

--- src/clj/csneps/snip_acting.clj
+++ src/clj/csneps/snip_acting.clj
@@ -1,6 +1,6 @@
 (in-ns 'csneps.snip)
 
-(declare adopt-subrules unadopt-subrules open-valve close-valve)
+(declare adopt-subrules unadopt-subrules add-valve-selector remove-valve-selector)
 
 ;;; CSNePS Acting System
 
@@ -17,7 +17,7 @@
       (when (isa? (syntactic-type-of policy) :csneps.core/CARule)
         (backward-infer policy taskid)
         (doseq [ich (@i-channels policy)]
-          (open-valve ich taskid))
+          (add-valve-selector ich {} (ct/currentContext) taskid))
         (adopt-subrules policy)
         taskid))))
 
@@ -36,14 +36,13 @@
       (when (isa? (syntactic-type-of policy) :csneps.core/CARule)
         (cancel-infer-of policy)
         (doseq [ich (@i-channels policy)]
-          (close-valve ich taskid))
+          (remove-valve-selector ich {} (ct/currentContext) taskid))
         (unadopt-subrules policy)))))
 
 (defn unadopt-subrules 
   [policy]
   (doseq [subrule (get (cf/dcsRelationTermsetMap policy) (slot/find-slot 'subrule))]
     (unadopt subrule)))
-  
 
 (defn attach-primaction [act fname]
   {:pre [(subtypep (semantic-type-of act) :Act)
