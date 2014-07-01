543d5a2b4715a45fbf916c6daf78e9d6f28475ea
Daniel R. Schlegel <drschleg@buffalo.edu>
2014-06-30 20:13:59 -0400

    Mapper benchmark initial version.

---
+++ src/clj/csneps/test/mapper_benchmark.clj
@@ -0,0 +1,63 @@
+(ns csneps.test.mapper-benchmark
+  (:require [csneps.core.contexts :as ct]
+            [csneps.core.caseframes :as cf]
+            [csneps.core.printer :as print]
+            [csneps.core.relations :as slot]
+            [csneps.core :as csneps]
+            [csneps.core.build :as build]
+            [csneps.snip :as snip]
+            [clojure.set :as set]
+            [csneps.core.snuser :as snuser]))
+
+
+(def runtime (atom 0))
+
+(defn log-elapsed
+  [start-time]
+  (swap! runtime + (/ (- (. java.lang.System (clojure.core/nanoTime)) start-time) 1000000.0)))
+
+(defn reset-benchmark
+  []
+  (def runtime (atom 0)))
+
+(defn print-time
+  []
+  (println "Run Time: " @runtime))
+
+(def adopt-order
+  '[[generalizeNN generalizeNNP generalizeNNPS generalizeNNS generalizeNP generalizeNPS]
+    [generalizeVBD generalizeVBG generalizeVBN generalizeVBP generalizeVB generalizeVBZ]
+    properNounToName
+    organizationHasName
+    nnName
+    nounPhraseToInstance
+    eventToInstance
+    pluralNounToGroup
+    subjAction
+    dobjAction
+    prepToRelation
+    nnToModifier
+    amodToModifier])
+
+(defn loadkb
+  [msgfile rulefile]
+  (load-file rulefile)
+  (load-file msgfile))
+
+(defn synsem-one-file
+  [msgfile rulefile]
+  (snuser/clearkb true)
+  (loadkb msgfile rulefile)
+  (let [start-time (. java.lang.System (clojure.core/nanoTime))]
+    (snuser/adopt-rules adopt-order)
+    (log-elapsed start-time)
+    (print-time)))
+
+(defn synsem-benchmark
+  [msgfolder rulefile]
+  (reset-benchmark)
+  (doseq [f (file-seq (clojure.java.io/file msgfolder))]
+    (synsem-one-file (.getPath f) rulefile)))
+
+  
+  
