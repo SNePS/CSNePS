7339ea6bf39558597d26c0b1d354a4e0e9f55b35
Daniel R. Schlegel <drschleg@buffalo.edu>
2019-03-17 21:20:04 -0400

    print-debug is now a macro to avoid any string processing in building the output unless the conditions are met for its execution. Removes the recently introduced need to wrap with (when (debug?) ... ) to achieve good performance.

--- src/clj/csneps/debug.clj
+++ src/clj/csneps/debug.clj
@@ -9,8 +9,10 @@
 (def screenprinter (agent nil))
 
 (defn println-agent
-  [& strs]
-  (send screenprinter (fn [_]  (println (clojure.string/join " " strs)))))
+  ([s]
+   (send screenprinter (fn [_] (println s))))
+  ([s & strs]
+   (println-agent (clojure.string/join " " (conj strs s)))))
 
 (def debug-features (ref #{}))
 (def debug-nodes (ref #{}))
@@ -27,18 +29,16 @@
      (set-debug-features ~@features)
      (set-debug-nodes ~@nodes)))
 
-(defn debug? [] (not (empty? @debug-features)))
-
-(defn print-debug
-  "Prints the message if any item from features is in debug-features, and 
-   if any of: any item in nodes is in debug-nodes or debug-nodes is empty 
+(defmacro print-debug
+  "Prints the message if any item from features is in debug-features, and
+   if any of: any item in nodes is in debug-nodes or debug-nodes is empty
    or nodes is empty. Accepts either a set of nodes/features, or a single
    one on their own."
-  [features nodes message]
+  [features nodes & message-strs]
   (let [features (if (seqable? features) (set features) #{features})
         nodes (if (and (seqable? nodes) (not (map? nodes))) (set nodes) #{nodes})] ;; Records are seqable.
     (when (and (not (empty? (set/intersection features @debug-features)))
-               (or (empty? @debug-nodes) 
+               (or (empty? @debug-nodes)
                    (empty? nodes)
                    (not (empty? (set/intersection nodes @debug-nodes)))))
-      (send screenprinter (fn [_]  (println message))))))
+      `(println-agent ~@message-strs))))
