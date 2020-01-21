6f3a5f9f217da011c44d95f7c27218450ff0f6d7
Daniel R. Schlegel <drschleg@buffalo.edu>
2020-01-20 21:14:26 -0500

    Additional tests.

--- test/Sneps3_Clojure/test/inference_graph.clj
+++ test/Sneps3_Clojure/test/inference_graph.clj
@@ -18,7 +18,7 @@
 ;;; Introduction Rules ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(deftest atomic-consensus-introduction
+(deftest consensus-introduction
   (snuser/assert 'a)
   (snuser/assert 'b)
   (snuser/defineTerm 'c)
@@ -33,25 +33,37 @@
     (is (= #{norterm} (snuser/askif '(nor d e))))
     (is (empty? (snuser/askif '(nor d c))))))
 
-(deftest atomic-andor-introduction
+(deftest disjunction-introduction
   (snuser/assert 'a)
   (snuser/assert 'b)
   (snuser/defineTerm 'c)
   (snuser/defineTerm 'd)
-  (snuser/defineTerm '(not e))
-  ;;; Or
   (let [andor-and (snuser/defineTerm '(or a b))
         andor-or (snuser/defineTerm '(or a c))]
     (is (= #{andor-and} (snuser/askif '(or a b))))
     (is (= #{andor-or} (snuser/askif '(or a c))))
-    (is (empty? (snuser/askif '(or c d)))))
-  ;; Xor
+    (is (empty? (snuser/askif '(or c d))))))
+
+(deftest xor-introduction
+  (snuser/assert 'a)
+  (snuser/assert 'b)
+  (snuser/defineTerm 'c)
+  (snuser/defineTerm '(not e))
   (let [andor-xor (snuser/defineTerm '(xor a e))]
     (is (= #{andor-xor} (snuser/askif '(xor a e))))
     (is (empty? (snuser/askif '(xor a b)))) ;; False
     (is (empty? (snuser/askif '(xor a c)))))) ;; Unknown
 
-(deftest atomic-thresh-introduction
+(deftest andor-introduction
+  (snuser/assert 'a)
+  (snuser/assert 'b)
+  (snuser/assert 'c)
+  (snuser/assert '(not f))
+  (snuser/assert '(not g))
+  (let [andor (snuser/defineTerm '(andor (3 5) a b c d e f g))]
+    (is (= #{andor} (snuser/askif '(andor (3 5) a b c d e f g))))))
+
+(deftest thresh-introduction
   (snuser/assert 'a)
   (snuser/assert 'b)
   (snuser/assert '(not c))
@@ -66,19 +78,26 @@
 ;;; Elimination Rules ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(deftest atomic-implication-elimination-backward-infer
+(deftest implication-elimination
   (snuser/assert '(if a b))
   (snuser/assert 'a)
   (is (= #{(snuser/find-term 'b)} (snuser/askif 'b))))
 
-(deftest generic-derivation-backward-infer
+(deftest implication-elimination-chaining
+  (snuser/assert 'a)
+  (snuser/assert '(if a b))
+  (snuser/assert '(if b c))
+  (snuser/assert '(if c d))
+  (is (= #{(snuser/find-term 'd)} (snuser/askif 'd))))
+
+(deftest generic-elimination
   (snuser/assert '(Isa (every x Cat) Animal))
   (snuser/assert '(Isa Glacier Cat))
   (snuser/assert '(Isa Fido Dog))
   (is (empty? (snuser/askif '(Isa Fido Animal))))
   (is (= #{(snuser/defineTerm '(Isa Glacier Animal))} (snuser/askif '(Isa Glacier Animal)))))
 
-(deftest atomic-negation-elimination
+(deftest negation-elimination
   (snuser/assert '(not (not a)))
   (is (= #{(snuser/defineTerm 'a)} (snuser/askif 'a)))
   (is (empty? (snuser/askif '(not a))))
@@ -92,3 +111,16 @@
   (is (= #{(snuser/find-term 'b)} (snuser/askif 'b)))
   (is (= #{(snuser/find-term 'c)} (snuser/askif 'c)))
   (is (empty? (snuser/askif 'd))))
+
+(deftest disjunction-elimination
+  (snuser/assert '(or a b c))
+  (snuser/assert '(not a))
+  (snuser/assert '(not b))
+  (is (= #{(snuser/find-term 'c)} (snuser/askif 'c)))
+  (is (empty? (snuser/askif 'b))))
+
+(deftest xor-elimination
+  (snuser/assert '(xor a b c))
+  (snuser/assert 'a)
+  (is (= #{(snuser/defineTerm '(not b))} (snuser/askif '(not b))))
+  (is (empty? (snuser/askif '(not a)))))
