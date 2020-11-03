aaab509ad71723230f0402bc14bc403137adf777
Daniel Schlegel <digitalneoplasm@gmail.com>
2020-11-02 20:42:15 -0500

    Recursively explores term to not leave molecular nodes without their children.

--- src/clj/csneps/utils/dotgraph.clj
+++ src/clj/csneps/utils/dotgraph.clj
@@ -1,6 +1,7 @@
 ;; Convert (part of) a CSNePS KB to a dot graph to be rendered with Graphviz.
 
 (ns csneps.utils.dotgraph
+  (:use [clojure.set])
   (:require [csneps.core :as csneps]
             [csneps.core.semantic-types :as st]
             [csneps.core.caseframes :as cf]
@@ -18,6 +19,12 @@
 
 (defn relation-to-dot [term slot fillers]
   (str/join "\n" (map #(filler-to-dot term slot %) fillers)))
+
+(defn restriction-to-dot [quantterm restriction]
+  (str (display-name quantterm) " -> " (display-name restriction) " [label=\"restriction\" style=\"dashed\""))
+
+(defn restrictions-to-dot [quantterm restrictions]
+  (str/join "\n" (map #(restriction-to-dot quantterm %) restrictions)))
 
 ;; Pre-supposes collapseable.
 (defn collapsed-relations-to-dot [term term-relations]
@@ -48,19 +55,30 @@
            (and (= 3 (count (:slots cf)))
                 (not (cf/quotedpp? cf)))))))
 
-(defn relations-to-dot [term term-relations collapse?]
+(defn relations-to-dot [term term-relations]
   (str/join "\n"
             (for [[slot fillers] term-relations]
                 (relation-to-dot term slot fillers))))
 
+;; Possible candidate to replace flatten-term - it's certainly nicer to look at. Performance?
+(defn terms-in-term [term]
+  (loop [terms #{}
+         to-check [term]]
+    (if (subset? (set to-check) terms) ;; Should we just check for empty?
+      terms
+      (recur (conj terms (first to-check))
+             (concat (rest to-check)
+                     (apply union (@csneps/down-cableset (first to-check)))
+                     (@csneps/restriction-set (first to-check)))))))
+
 (defn terms-to-dot [terms collapse?]
-  (str/join "\n"
-            (for [term terms
-                  :let [term (csneps/get-term term)
-                        term-relations (cf/dcsRelationTermsetMap term)]]
-              (if (and collapse? (collapseable? term))
-                (collapsed-relations-to-dot term term-relations)
-                (relations-to-dot term term-relations collapse?)))))
+  (let [terms (map csneps/get-term terms)]
+    (str/join "\n"
+              (for [term (apply union (map terms-in-term terms)) ;; Get all subterms so we don't omit anything!
+                    :let [term-relations (cf/dcsRelationTermsetMap term)]]
+                (if (and collapse? (collapseable? term))
+                  (collapsed-relations-to-dot term term-relations)
+                  (relations-to-dot term term-relations))))))
 
 (defn generate-dotfile [terms fname collapse?]
   (let [termstr (terms-to-dot terms collapse?)]
