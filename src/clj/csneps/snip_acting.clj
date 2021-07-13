84ba0d20038f7dd3f97699b3037c30d4f65a4af7
Daniel R. Schlegel <drschleg@buffalo.edu>
2021-07-12 21:44:00 -0400

    Initial port of perform.

--- src/clj/csneps/snip_acting.clj
+++ src/clj/csneps/snip_acting.clj
@@ -44,13 +44,40 @@
   (doseq [subrule (get (cf/dcsRelationTermsetMap policy) (slot/find-slot 'subrule))]
     (unadopt subrule)))
 
-;; TODO
-(defmacro define-primaction [name vars & forms]
+;; This is a fairly direct port of the Sneps3 version, but I'm not sure it does everything we'd like it to.
+;; Further analysis of the SNePS 2 SNeRE and other such systems should be performed before this is actively used.
+(defmacro define-primaction
+  "Creates the function definition of the primitive action named name.
+    vars should be a (possibly empty) list of slot names
+      that get bound to the appropriate nodes.
+    However, if any var is enclosed in parentheses,
+       it gets bound to a member of the appropriate node set.
+    forms syntax is just as it is for standard Clojure function definition."
+  [name vars & forms]
 
   )
 
-;; TODO
-(defn perform [actform])
+(defn perform
+  "actform will be defined as an Act term (which might already exist).
+      If there is a primitive action function for that act, it will be applied to the act;
+      Else if the act has an actions slot
+          and there is a primitive action function for that action, it will be applied to the act;
+      Else an error will be raised."
+  [actform]
+  (let [act (build/build actform :Act)
+        todo (pb-findtos act 'actions)
+        action (first todo)]
+    (cond
+      ;; This act has a primitive action associated with it.
+      (fn? (@primaction act))
+      ((@primaction act) act)
+      ;; There is an action slot, and a function for that action.
+      (and action
+           (fn? (@primaction action)))
+      ((@primaction action) act)
+      ;; Error.
+      :default
+      (error "I don't know how to perform " act "."))))
 
 (defn attach-primaction [act fname]
   {:pre [(subtypep (st/semantic-type-of act) :Act)
