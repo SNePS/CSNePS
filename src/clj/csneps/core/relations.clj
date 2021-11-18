9630d822bbbedb62e516341334810fb4ae8e8c41
Daniel R. Schlegel <drschleg@buffalo.edu>
2021-11-18 17:25:53 -0500

    Added restriction as a reserved word so slots cannot be named it. This will eventually be used in path-based inference to follow restrictions into arbs/inds.

--- src/clj/csneps/core/relations.clj
+++ src/clj/csneps/core/relations.clj
@@ -47,10 +47,14 @@
                 min 1
                 path (ref nil)}}]
   {:pre [(symbol? name)]} ;"Slot name must be a symbol."
-  (if (find-slot name :errorp false)
+  (cond
+    (find-slot name :errorp false)
     (do
       (println "A slot with name: " name " is already defined. Using existing definition.")
       (find-slot name))
+    (= name 'restriction)
+    (error (str "'" name "' is a reserved word and not a valid name for a slot."))
+    :else
     (do
       (assert (csneps.core/semantic-type-p (keyword type))) ;"The type given is not a valid semantic type"
       (assert (string? docstring))
